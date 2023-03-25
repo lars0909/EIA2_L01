1c4de8259860c9d7a569734f25ef4cc3b39ac9e0
Lars <lars.riehle@outlook.de>
2023-03-25 18:05:50 +0100

    sad

--- script.js
+++ script.js
@@ -8,33 +8,43 @@ Quellen: Fisher Yates Shuffle
 let subjekt = ["Obi Wan ", "Anakin ", "Yoda ", "Darth Vader ", "Grogu "];
 let prädikat = ["zerstört ", "liebt ", "hasst ", "verehrt ", "verachtet "];
 let objekt = ["Raumschiffe", "Planeten", "die Sith", "die Jedi", "Kinder"];
-let order = [0, 1, 2, 3, 4];
-let subjekt_reihenfolge = [];
-let verb_reihenfolge = [];
-let objekt_reihenfolge = [];
-function rdmindex() {
-    let b = 5, randomIndex;
-    while (b != 0) {
-        randomIndex = Math.floor(Math.random() * b);
-        b--;
-        [order[b], order[randomIndex]] = [order[randomIndex], order[b]];
-    }
-    return order;
+// console.log(subjekt);
+// console.log(prädikat);
+// console.log(objekt);
+for (let i = 5; i >= 1; i--) {
+    console.log(i);
+    console.log(getVerse(subjekt, prädikat, objekt));
 }
-function readpoem() {
-    for (let i = order.length - 1; i >= 0; i--) {
-        console.log(subjekt[subjekt_reihenfolge[i]] + prädikat[verb_reihenfolge[i]] + objekt[objekt_reihenfolge[i]]);
-    }
+function getVerse(_subjekt, _prädikat, _objekt) {
+    let test = "jesfhjdsh";
+    return (test);
 }
-;
-function rdmorder() {
-    rdmindex();
-    subjekt_reihenfolge = [...order];
-    rdmindex();
-    verb_reihenfolge = [...order];
-    rdmindex();
-    objekt_reihenfolge = [...order];
-    readpoem();
-}
-rdmorder();
+// let order = [0, 1, 2, 3, 4];
+// let subjekt_reihenfolge: number[] = [];
+// let verb_reihenfolge: number[] = [];
+// let objekt_reihenfolge: number[] = [];
+// function rdmindex(): number[] {         //FIsher Yates Shuffle
+//     let b = 5, randomIndex;
+//     while (b != 0) {
+//         randomIndex = Math.floor(Math.random() * b);
+//         b--;
+//         [order[b], order[randomIndex]] = [order[randomIndex], order[b]];
+//     }
+//     return order;
+// }
+// function readpoem() {
+//     for (let i = order.length - 1; i >= 0; i--) {
+//         console.log(subjekt[subjekt_reihenfolge[i]] + prädikat[verb_reihenfolge[i]] + objekt[objekt_reihenfolge[i]]);
+//     }
+// };
+// function rdmorder() {
+//     rdmindex();
+//     subjekt_reihenfolge = [...order];
+//     rdmindex();
+//     verb_reihenfolge = [...order];
+//     rdmindex();
+//     objekt_reihenfolge = [...order];
+//     readpoem();
+// }
+// rdmorder();
 //# sourceMappingURL=script.js.map
--- script.ts
+++ script.ts
@@ -8,34 +8,55 @@ Quellen: Fisher Yates Shuffle
 let subjekt: string[] = ["Obi Wan ", "Anakin ", "Yoda ", "Darth Vader ", "Grogu "];
 let prädikat: string[] = ["zerstört ", "liebt ", "hasst ", "verehrt ", "verachtet "];
 let objekt: string[] = ["Raumschiffe", "Planeten", "die Sith", "die Jedi", "Kinder"];
-let order = [0, 1, 2, 3, 4];
-let subjekt_reihenfolge: number[] = [];
-let verb_reihenfolge: number[] = [];
-let objekt_reihenfolge: number[] = [];
+let vers: string = "";
+// console.log(subjekt);
+// console.log(prädikat);
+// console.log(objekt);
 
-function rdmindex(): number[] {         //FIsher Yates Shuffle
-    let b = 5, randomIndex;
-    while (b != 0) {
-        randomIndex = Math.floor(Math.random() * b);
-        b--;
-        [order[b], order[randomIndex]] = [order[randomIndex], order[b]];
-    }
-    return order;
+for (let i =5 ; i >= 1; i--) {
+        //console.log(i); 
+        //console.log(getVerse(subjekt, prädikat, objekt));
 }
 
-function readpoem() {
-    for (let i = order.length - 1; i >= 0; i--) {
-        console.log(subjekt[subjekt_reihenfolge[i]] + prädikat[verb_reihenfolge[i]] + objekt[objekt_reihenfolge[i]]);
-    }
-};
-
-function rdmorder() {
-    rdmindex();
-    subjekt_reihenfolge = [...order];
-    rdmindex();
-    verb_reihenfolge = [...order];
-    rdmindex();
-    objekt_reihenfolge = [...order];
-    readpoem();
+function getVerse(_subjekt: string[], _prädikat: string[], _objekt: string[]): string {
+    let rdmNum1: number = Math.floor(Math.random() * _subjekt.length);
+    let rdmNum2: number = Math.floor(Math.random() * _prädikat.length);
+    let rdmNum3: number = Math.floor(Math.random() * _objekt.length);
+    let test = "jesfhjdsh"
+    return(test);
 }
-rdmorder();
+
+
+
+
+// let order = [0, 1, 2, 3, 4];
+// let subjekt_reihenfolge: number[] = [];
+// let verb_reihenfolge: number[] = [];
+// let objekt_reihenfolge: number[] = [];
+
+// function rdmindex(): number[] {         //FIsher Yates Shuffle
+//     let b = 5, randomIndex;
+//     while (b != 0) {
+//         randomIndex = Math.floor(Math.random() * b);
+//         b--;
+//         [order[b], order[randomIndex]] = [order[randomIndex], order[b]];
+//     }
+//     return order;
+// }
+
+// function readpoem() {
+//     for (let i = order.length - 1; i >= 0; i--) {
+//         console.log(subjekt[subjekt_reihenfolge[i]] + prädikat[verb_reihenfolge[i]] + objekt[objekt_reihenfolge[i]]);
+//     }
+// };
+
+// function rdmorder() {
+//     rdmindex();
+//     subjekt_reihenfolge = [...order];
+//     rdmindex();
+//     verb_reihenfolge = [...order];
+//     rdmindex();
+//     objekt_reihenfolge = [...order];
+//     readpoem();
+// }
+// rdmorder();
